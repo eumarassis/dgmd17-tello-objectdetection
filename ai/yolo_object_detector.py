cad5fb9dcd6fd257adffd8d4396c1ea3523175a0
eumarassis <eumarassis@live.com>
2022-07-25 09:57:55 -0400

    Added Yolo Integration

--- ai/yolo_object_detector.py
+++ ai/yolo_object_detector.py
@@ -3,16 +3,19 @@
 import numpy as np
 from typing import List, Dict, Tuple, Optional
 from ai.object_detector import ObjectDetector
+import torch
+from PIL import Image
 
 class YOLOObjectDetector (ObjectDetector):
     """YOLO Object Detector Class"""
 
     def __init__(self):
-        pass
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
 
     def detect_people (self, image: np.ndarray) -> np.ndarray:
         """Detect people and return bounding boxes of all people in the given image"""
-        return np.ones((10, 10))
+        return self.model(image)
+
 
     def measure_distance (self, image: np.ndarray, bounding_boxes: np.ndarray)-> np.ndarray:
         """Return distance between bounding Boxes of all people in the given image"""
@@ -20,5 +23,6 @@ class YOLOObjectDetector (ObjectDetector):
 
     def draw_bounding_boxes (self,  image: np.ndarray, bounding_boxes: np.ndarray, color: str, border: int  ) -> np.ndarray:
         """Draw bounding boxes in the given image"""
-        return image
+        bounding_boxes.render()
+        return Image.fromarray( bounding_boxes.imgs[0] )
 
